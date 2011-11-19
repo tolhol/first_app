5a521799f97fd1bc77e8140a19506a9f3a4c9471
tolhol <tolhol@yandex.ru>
2011-11-20 00:06:56 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tolhol::Application.config.secret_token = '06c5ad43ed2689f2dd482c6c058cc9edbc5364bd255e4c7c64f7f7c679ac3fb41106e8ca5f1bf0827e6d711b5809c98daeb739f1e051fa3564a36c56619126be'
